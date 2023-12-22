068a65d23a51bc50fceff6aabb1ff97449d2eec7
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2023-12-22 01:36:31 +0000

    new changes

--- app.js
+++ app.js
@@ -12,10 +12,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-/*
-const button = document.querySelector("#header");
-
-button.addEventListener("click", () => {
-  alert("you are hired");
-});
-*/
